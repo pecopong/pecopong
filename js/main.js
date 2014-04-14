c6457e8d25890817db39797d6180b50cc084e781
pecopong <pecopong90@gmail.com>
2014-04-14 16:32:24 +0900

    Merge branch 'gh-pages' of https://github.com/pecopong/pecotype into gh-pages
    Conflicts:
    css/style.css
    index.html
    js/main.js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 var globalparent;
 
 var init = function(){
@@ -53,3 +54,58 @@ var addDiv = function(text){
 
 
 window.onload=init;
+=======
+var test = ['나는','에코를','원한다'];
+var i = 0;
+var a =''; 
+var xc = '';
+var yc = '';
+var xco = '';
+var yco = '';
+var tts = new GoogleTTS('ko-KR');
+
+myVolume = 1;
+
+var touch = function(){
+    var el = document.createElement('div');
+    el.innerHTML = test[i];
+    el.className = i;
+    
+    if(i<test.length){
+       
+        document.body.appendChild(el);
+    }
+    else{
+        console.log('The End');
+    }
+    xco = xc+'px';
+    yco = yc+'px';
+    
+    
+    a ='.'+i.toString();
+    
+    $(a).css({'position':'absolute',
+              'font-size':'30px',
+              'left':xco,
+              'top':yco,
+              'height':'30px',
+              'width':'30px'
+             })
+        .fadeOut(8000);
+    
+    
+         tts.play(test[i]);
+     
+      
+       i++
+}
+
+
+$(document).click(function(e){
+     xc = e.pageX;
+     yc = e.pageY;
+     
+     touch();
+
+})
+>>>>>>> 2dbdbd5318198bf204a06ef401256610233b9705
